aaf280439f30bd221da2988b7766050274613ad4
Romu9319 <129613538+Romu9319@users.noreply.github.com>
2023-06-17 11:45:11 +0000

    funcion de generador de escusas corregida

--- src/app.js
+++ src/app.js
@@ -5,6 +5,18 @@ import "./index.html";
 
 window.onload = function() {
   //write your code here
+
+  function generador(quien, verbo, que, cuando) {
+    let excusa = "";
+
+    excusa = quien[Math.floor(Math.random() * quien.length)];
+    excusa += " " + verbo[Math.floor(Math.random() * verbo.length)];
+    excusa += " " + que[Math.floor(Math.random() * que.length)];
+    excusa += " " + cuando[Math.floor(Math.random() * cuando.length)];
+
+    return excusa;
+  }
+
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "the keys", "the car"];
@@ -15,28 +27,8 @@ window.onload = function() {
     "during my lunch",
     "while I was praying"
   ];
-  let the_who = 0;
-  let the_action = 0;
-  let the_what = 0;
-  let the_when = 0;
 
-  function onLoad(x) {
-    let select_who = Math.floor(Math.random() * 4);
-    the_who = who[select_who];
-
-    let select_action = Math.floor(Math.random() * 4);
-    the_action = action[select_action];
-
-    let select_what = Math.floor(Math.random() * 3);
-    the_what = what[select_what];
-
-    let select_when = Math.floor(Math.random() * 5);
-    the_when = when[select_when];
-
-    return the_who + " " + the_action + " " + the_what + " " + the_when;
-  }
-
-  let resultado = onLoad();
+  let resultado = generador(who, action, what, when);
 
   let my_excuse = document.getElementById("excuse");
 
